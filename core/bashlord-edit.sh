8012656d8f0c99270859399bcd80cd3006fedb21
vlord <vlord@free.fr>
2009-05-04 22:27:11 +0000

    GPL3 terms added in core scripts, numerous minor bugs fixed, help command more detailed, import command added

--- core/bashlord-edit.sh
+++ core/bashlord-edit.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+
+#    Bashlord, a bash scripts management tool
+#    Copyright (C) 2009 Vladimir Bueb — All rights reserved
+#
+#
+#
+#    This file is part of Bashlord.
+#
+#    Bashlord is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Bashlord is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Bashlord.  If not, see <http://www.gnu.org/licenses/>.
+
 SCRIPT=$BASHLORD_HOME/scripts/$1.sh 
 
 #if the script file we want to edit doesn't exists
--- core/bashlord-help.sh
+++ core/bashlord-help.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
+
+#    Bashlord, a bash scripts management tool
+#    Copyright (C) 2009 Vladimir Bueb — All rights reserved
+#
+#
+#
+#    This file is part of Bashlord.
+#
+#    Bashlord is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Bashlord is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Bashlord.  If not, see <http://www.gnu.org/licenses/>.
+
+
+echo "Usage:"
+echo "  bashlord <command> [command parameters]"
+echo "  blord <command> [command parameters]"
+echo
+echo "Available commands:"
 #echo all bashlord core scripts available as bashlord command parameter
 for SCRIPT in `ls $BASHLORD_HOME/core/bashlord-*.sh | xargs -n1 basename`
 do
-	echo $SCRIPT | sed '/\.sh/s///g' | sed '/bashlord-/s///g'
+	echo "  $SCRIPT" | sed '/\.sh/s///g' | sed '/bashlord-/s///g'
 done
