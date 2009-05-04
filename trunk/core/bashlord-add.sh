09a7e54f36da1bc940a41c12708cb7d2009da109
vlord@free.fr <vlord@free.fr@5e061432-35db-11de-b310-9106b1f7c239>
2009-05-04 22:27:11 +0000

    GPL3 terms added in core scripts, numerous minor bugs fixed, help command more detailed, import command added

--- trunk/core/bashlord-add.sh
+++ trunk/core/bashlord-add.sh
@@ -23,23 +23,24 @@
 NEW_SCRIPT=$BASHLORD_HOME/scripts/$1.sh 
 ALIAS=$1
 
-#if the script file we want to create doesn't exists yet
-if [ ! -e $NEW_SCRIPT ]
+#if the script file we want to create already exists we return an error
+if [ -e $NEW_SCRIPT ]
 then
-	#create script file
-	echo "#!/bin/bash" > $NEW_SCRIPT
-	echo "echo I am a new bashlord script, type the following command to edit the script:" >> $NEW_SCRIPT
-	echo "echo bashlord edit $1" >> $NEW_SCRIPT
-	chmod u+x $NEW_SCRIPT
-
-	#add alias
-	echo "alias $ALIAS='$NEW_SCRIPT'" >> $BASHLORD_HOME/conf.d/aliases
-
-	#regenerate config
-	. $BASHLORD_HOME/core/bashlord-reload.sh
-
-else
-	#if the script already exists we return an error
         echo "Script $NEW_SCRIPT already exists !"	
 	return 1;		
 fi
+
+#create script file
+echo "#!/bin/bash" > $NEW_SCRIPT
+echo "echo I am a new bashlord script, type the following command to edit the script:" >> $NEW_SCRIPT
+echo "echo bashlord edit $1" >> $NEW_SCRIPT
+chmod u+x $NEW_SCRIPT
+
+#add alias
+echo "alias $ALIAS='$NEW_SCRIPT'" >> $BASHLORD_HOME/conf.d/aliases
+
+#output success message
+echo "Script $ALIAS added"
+
+#regenerate config
+. $BASHLORD_HOME/core/bashlord-reload.sh
--- trunk/core/bashlord-delete-all.sh
+++ trunk/core/bashlord-delete-all.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+
+
+#    Bashlord, a bash scripts management tool
+#    Copyright (C) 2009 Vladimir Bueb — All rights reserved
+#
+#
+#
+#    This file is part of Bashlord.
+#
+#    Bashlord is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Bashlord is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Bashlord.  If not, see <http://www.gnu.org/licenses/>.
+
 echo "Type 'yes' to confirm you really want to erase all your bashlord's scripts"
 read CONFIRM
 
@@ -16,7 +38,8 @@ rm -rf $BASHLORD_HOME/scripts/*
 #removing aliases
 > $BASHLORD_HOME/conf.d/aliases
 
+#output success message
+echo "All scripts have been deleted"
+
 #reload config
 . $BASHLORD_HOME/core/bashlord-reload.sh
-
-echo "All scripts have been erased"
